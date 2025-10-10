5f502801c57e5fc1bb7fd5d50f553913c26bc64c
WayneGretzky1 <brockbye@outlook.com>
2025-10-10 18:22:02 -0500

    longest palindrome

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -1,23 +1,39 @@
-def longestPalindrome(self, s):
-    """
-    :type s: str
-    :rtype: str
-    """
-    def expandAroundCenter(left, right):
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
-        return s[left + 1:right]
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def isPalindrome(s):
+            l = 0
+            r = len(s) - 1
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l+=1
+                r-=1
+            return True
+        
+        n = len(s)
+        dp = [[False for _ in range(n)] for _ in range(n)]
+        start = 0
+        max_len = 1
 
-    longest = ""
-    for i in range(len(s)):
-        # Odd length palindrome
-        temp = expandAroundCenter(i, i)
-        if len(temp) > len(longest):
-            longest = temp
-        # Even length palindrome
-        temp = expandAroundCenter(i, i + 1)
-        if len(temp) > len(longest):
-            longest = temp
+        for i in range(n):
+            dp[i][i] = True
 
-    return longest
+        for i in range(n-1):
+            if s[i] == s[i+1]:
+                dp[i][i+1] = True
+                start = i
+                max_len = 2
+        
+        for length in range(3, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j] and dp[i+1][j-1] == True:
+                    dp[i][j] = True
+                    start = i
+                    max_len = length
+
+        return s[start:start+max_len]
