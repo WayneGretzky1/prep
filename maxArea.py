c8afe5b05998a929a6f1dd664d2697190bd521b0
WayneGretzky1 <brockbye@outlook.com>
2025-10-16 15:03:03 -0500

    maxArea

---
+++ maxArea.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+
+        max_area = 0
+        l, r = 0, len(height) - 1
+
+        while l < r:
+            w = r - l
+            h = min(height[l], height[r])
+            max_area = max(max_area, w * h)
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return max_area
